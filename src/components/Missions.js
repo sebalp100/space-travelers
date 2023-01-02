81a7850ded71f177f21aa2d4635a2dc3ffd57779
sebalp100 <sebalp100@gmail.com>
2023-01-02 16:37:15 -0300

    Add missions page basic style and fetch data

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Missions = () => (
-  <div className="listContainer">
-    <div className="bookDiv">
-      <p>Hi</p>
+const Mission = () => {
+  const missions = useSelector((state) => state.missionsReducer.missions);
+
+  return (
+    <div>
+      {missions.loading && <div>Loading...</div>}
+      {!missions.loading && missions.error ? (
+        <div>
+          Error:
+          {missions.error}
+        </div>
+      ) : null}
+      {!missions.loading && missions.length ? (
+        <div className="tableContainer">
+          <table>
+            <thead>
+              <tr className="head">
+                <th className="name">Mission</th>
+                <th className="desc">Description</th>
+                <th>Status</th>
+                <th>Joined</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                missions.map((user) => (
+                  <tr key={user.mission_id}>
+                    <td key={user.mission_id}>{user.mission_name}</td>
+                    <td key={user.description}>{user.description}</td>
+                    <td className="member"><button type="button">Not A Member</button></td>
+                    <td>J</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+      ) : null}
     </div>
-    <div className="bar" />
-  </div>
-);
-export default Missions;
+  );
+};
+
+export default Mission;
