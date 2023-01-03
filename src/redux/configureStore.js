03cc74274699a6d3524522c997eac5a517833bc8
SimonGideon <simongideon918@gmail.com>
2023-01-03 22:58:34 +0300

    Solve linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,8 @@
 import logger from 'redux-logger';
 import missionsReducer from './missions/missionSlice';
-const { configureStore } = require('@reduxjs/toolkit');
 import rocketReducer from './rocket/rocketSlice';
+
+const { configureStore } = require('@reduxjs/toolkit');
 
 const store = configureStore({
   reducer: {
