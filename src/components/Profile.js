3f3ac953890597d6e4b8b3ef6ee48bdb42223479
SimonGideon <simongideon918@gmail.com>
2023-01-05 22:34:10 +0300

    Reserved Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRocket } from '../redux/rocket/rocketSlice';
+import '../styles/myProfile.css';
 
-const Profile = () => (
-  <div className="listContainer">
-    <div className="bookDiv">
-      <p>My Profile</p>
-    </div>
-    <div className="bar" />
-  </div>
-);
-export default Profile;
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rocketData);
+
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(getRocket());
+    }
+  }, [dispatch, rockets.length]);
+
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  return (
+    <section>
+      <div className="container">
+        <h2>My Rockets</h2>
+        {reservedRockets.length === 0
+          ? <p>No reserved rockets</p> : (
+            <ul>
+              {reservedRockets.map((rocket) => (
+                <li key={rocket.rocket_id}>{rocket.rocket_name}</li>
+              ))}
+            </ul>
+          )}
+      </div>
+    </section>
+  );
+};
+
+export default MyProfile;
