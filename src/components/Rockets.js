e3ce95fcd419e53ec891603f4550e26e80f5a24e
SimonGideon <simongideon918@gmail.com>
2023-01-05 03:00:52 +0300

    Display-actrions-Reservation

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRocket } from '../redux/rocket/rocketSlice';
+import { getRocket, updateRocket } from '../redux/rocket/rocketSlice';
+import '../styles/rockets.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
@@ -12,14 +13,37 @@ const Rockets = () => {
     }
   }, [dispatch, rockets.length]);
 
+  const handleClick = (id, reserved) => {
+    dispatch(updateRocket({ id, newState: reserved ? ' ' : 'link' }));
+  };
+
   return (
-    <div>
-      {rockets.map((e) => (
-        <div key={e.rocket_id}>
-          <div>{e.rocket_id}</div>
-          <div>{e.rocket_name}</div>
-          <div>{e.description}</div>
-          <div>{e.images}</div>
+    <div className="frame">
+      {rockets.map((rocket) => (
+        <div key={rocket.rocket_id}>
+          <div className="rocket-container">
+            <div className="image-container">
+              <img src={rocket.images} alt={rocket.rocket_name} />
+            </div>
+            <div className="rocket-details">
+              <h2 className="rocket-title">{rocket.rocket_name}</h2>
+              <p className="rockets-description">
+                {rocket.reserved && (
+                <span className="rocket-badge">
+                  Reserved
+                </span>
+                )}
+                {' '}
+                {rocket.description}
+              </p>
+              <br />
+              {!rocket.reserved ? (
+                <button type="button" className="rocket-reserve-btn" onClick={() => handleClick(rocket.rocket_id, rocket.reserved)}>Reserve Rocket</button>
+              ) : (
+                <button type="button" className="rocket-cancel-btn" onClick={() => handleClick(rocket.rocket_id, rocket.reserved)}>Cancel Reservation</button>
+              )}
+            </div>
+          </div>
         </div>
       ))}
     </div>
